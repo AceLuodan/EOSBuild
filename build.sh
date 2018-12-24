d3896f69871d5a76ab3a11a10c8eff94ad7da6c1
AceLuodan <903585177@qq.com>
2018-12-24 13:59:50 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,8 +9,6 @@ git submodule update --init --recursive
 （2）切换到v1.4.1
 
 git checkout -b v1.4.1 v1.4.1
---------------------- 
-作者：w7849516230 
-来源：CSDN 
+
 原文：https://blog.csdn.net/w7849516230/article/details/80781173 
-版权声明：本文为博主原创文章，转载请附上博文链接！
+
