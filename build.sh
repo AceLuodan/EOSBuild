44185b926e0a48d2d0b632fef0cfa49d2d1d9cc8
AceLuodan <903585177@qq.com>
2018-12-24 13:59:33 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+1）下载代码
+
+git clone https://github.com/EOSIO/eos --recursive
+
+如果忘记使用“–recursive”，可在克隆下的eos目录中执行
+
+git submodule update --init --recursive
+1
+（2）切换到v1.4.1
+
+git checkout -b v1.4.1 v1.4.1
+--------------------- 
+作者：w7849516230 
+来源：CSDN 
+原文：https://blog.csdn.net/w7849516230/article/details/80781173 
+版权声明：本文为博主原创文章，转载请附上博文链接！
